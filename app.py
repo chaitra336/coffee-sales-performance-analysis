d77396f4e2b70ce1c56a9c5936077c5a02f89a8e
chaitra336 <chaitrabh123@gmail.com>
2026-03-28 11:24:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ The goal is to support better decision-making in staffing, operations, and sales
 
 
 # Load dataset
-df = df = pd.read_excel("coffee_sales_cleaned(1).xls")
+df = df = pd.read_excel("coffee_sales_cleaned (1).xls")
 
 # Convert transaction_time to datetime
 df["transaction_time"] = pd.to_datetime(df["transaction_time"], errors="coerce")
